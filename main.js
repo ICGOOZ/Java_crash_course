7663b4b57cd2c4ff65190366fc0a6b1aa9998e60
ICGOOZ <jwangsgaard97@s.tooeletech.edu>
2024-11-05 12:24:54 -0700

    finished the course

---
+++ main.js
@@ -0,0 +1,10 @@
+const ul = document.querySelector(".items");
+function greetUser(name) {
+  console.log("Hello, " + name + "!");
+}
+let name = "John"; // String
+let age = 30; // Number
+let isStudent = true; // Boolean
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Is Student: " + isStudent);
